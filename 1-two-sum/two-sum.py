70840846222fb185818ef981ffa1e74369578041
Rachana Potham <156023447+rachanapotha@users.noreply.github.com>
2025-11-02 14:10:05 -0600

    Time: 0 ms (100.00%) | Memory: 18.9 MB (33.80%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        num_map = {}  # stores value: index
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+
